e0689e976cac2a7d4a32319d6dad77f14229e3f5
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-09 10:35:41 +1000

    🎉 Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,8 @@
-import { defineConfig } from 'astro/config';
-// https://astro.build/config
-export default defineConfig({});
+import { defineConfig } from "astro/config";
+
+import react from "@astrojs/react";
+
+export default defineConfig({
+    tailwindConfig: "./tailwind.config.js",
+    integrations: [react()],
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-    content: ["./index.html"],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
+    mode: "jit",
+    purge: ["./public/**/*.html", "./src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}"],
 };
