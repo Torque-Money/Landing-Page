7320aeea569745109d7e05f4d93489e9e4d06987
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-11 08:51:30 +1000

    🎉 Update

--- src/components/Text/Paragraph/index.tsx
+++ src/components/Text/Paragraph/index.tsx
@@ -1,4 +1,4 @@
 export default function Paragraph({ side, children }: { side: "left" | "right"; children: any }) {
-    if (side === "left") return <p class="text-xl text-neutral-400 font-medium mt-5 w-11/12 mr-auto">{children}</p>;
-    else return <p class="text-xl text-neutral-400 font-medium mt-5 w-11/12 ml-auto">{children}</p>;
+    if (side === "left") return <p className="text-xl text-neutral-400 font-medium mt-5 w-11/12 mr-auto">{children}</p>;
+    else return <p className="text-xl text-neutral-400 font-medium mt-5 w-11/12 ml-auto">{children}</p>;
 }
