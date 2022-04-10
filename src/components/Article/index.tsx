dccae5190d8e120aebe748aa012f01e5649e5c3d
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-11 09:53:34 +1000

    🎉 Update

--- src/components/Article/index.tsx
+++ src/components/Article/index.tsx
@@ -1,30 +1,40 @@
-export default function Article({ side }: { side: "left" | "right" }) {
+import React from "react";
+
+export default function Article({ side, heading, text, cta, image }: { side: "left" | "right"; heading: any; text: any; cta: any; image: any }) {
     return (
-        <article class="w-4/5 mx-auto flex xl:flex-row flex-col xl:items-start items-center justify-evenly xl:space-x-32 xl:space-y-0 space-y-24">
+        <article className="w-4/5 mx-auto flex xl:flex-row flex-col xl:items-start items-center justify-evenly xl:space-x-32 xl:space-y-0 space-y-24">
             {side === "left" ? (
                 <>
-                    <div class="flex flex-col items-start space-y-12 xl:pt-24" style="min-width: 400px">
-                        <div class="pb-6 border-b-2 border-b-neutral-400 border-opacity-5">
-                            <h3 class="text-white font-bold text-3xl">
-                                <slot name="heading" />
+                    <div className="flex flex-col items-start space-y-12 xl:pt-24" style={{ minWidth: 400 }}>
+                        <div className="pb-6 border-b-2 border-b-neutral-400 border-opacity-5">
+                            {/* <h3 className="text-white font-bold text-3xl"><slot name="heading" /></h3> */}
+                            <h3 className="text-white font-bold text-3xl">
+                                <heading />
                             </h3>
-                            <slot name="text" />
+                            {/* <slot name="text" /> */}
+                            <text />
                         </div>
-                        <slot name="cta" />
+                        <cta />
+                        {/* <slot name="cta" /> */}
                     </div>
-                    <slot name="image" />
+                    {/* <slot name="image" /> */}
+                    <image />
                 </>
             ) : (
                 <>
-                    <slot name="image" />
-                    <div class="flex flex-col items-end space-y-12 xl:pt-24" style="min-width: 400px">
-                        <div class="text-right pb-6 border-b-2 border-b-neutral-400 border-opacity-5">
-                            <h3 class="text-white font-bold text-3xl">
-                                <slot name="heading" />
+                    {/* <slot name="image" /> */}
+                    <image />
+                    <div className="flex flex-col items-end space-y-12 xl:pt-24" style={{ minWidth: 400 }}>
+                        <div className="text-right pb-6 border-b-2 border-b-neutral-400 border-opacity-5">
+                            {/* <h3 className="text-white font-bold text-3xl"><slot name="heading" /></h3> */}
+                            <h3 className="text-white font-bold text-3xl">
+                                <heading />
                             </h3>
-                            <slot name="text" />
+                            {/* <slot name="text" /> */}
+                            <text />
                         </div>
-                        <slot name="cta" />
+                        {/* <slot name="cta" /> */}
+                        <cta />
                     </div>
                 </>
             )}
--- src/components/Text/Paragraph/index.tsx
+++ src/components/Text/Paragraph/index.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 export default function Paragraph({ side, children }: { side: "left" | "right"; children: any }) {
     if (side === "left") return <p className="text-xl text-neutral-400 font-medium mt-5 w-11/12 mr-auto">{children}</p>;
     else return <p className="text-xl text-neutral-400 font-medium mt-5 w-11/12 ml-auto">{children}</p>;
