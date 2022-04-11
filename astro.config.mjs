118f3826f6529b35edc0802bfba6f8ad6390b5c3
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-11 14:46:27 +1000

    🎉 Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ const config = {
     description: "Home of high leverage and the highest sustainable yield farming rewards, powered 100% by DeFi.",
     themeColor: "#921ebb",
     siteURL: "https://torque.money",
-    bannerURL: "https://i.imgur.com/IzQ9Jvl.png",
+    bannerURL: "https://i.imgur.com/voyXWCK.png",
     twitterHandle: "@torque_money",
     connectURL: "https://linktr.ee/torque_money",
     appURL: "https://app.torque.money",
