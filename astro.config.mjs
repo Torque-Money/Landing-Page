dd75cc15ff1b7a3c80b01cc27cffeb3a0026a5bf
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-11 12:22:03 +1000

    🎉 Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,16 @@
 import { defineConfig } from "astro/config";
 
+const config = {
+    title: "Torque Money - DeFi The Norm",
+    description: "Home of high leverage and the highest sustainable yield farming rewards, powered 100% by DeFi.",
+    themeColor: "#921ebb",
+    siteURL: "https://torque.money",
+    bannerURL: "https://i.imgur.com/IzQ9Jvl.png",
+    twitterHandle: "@torque_money",
+};
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://torque.money/",
+    misc: config,
+    site: config.siteURL,
 });
